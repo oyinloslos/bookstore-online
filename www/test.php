bf71b721cc6598d2bd2436415d84e6077141d967
Adedoyin Onabanjo <oyinorobo@gmail.com>
2017-03-29 00:11:25 +0100

    pictures uploading

--- www/test.php
+++ www/test.php
@@ -21,6 +21,56 @@ if(array_key_exists('save', $_POST)){
 	print_r($_FILES);
 }
 
+#max file size...
+define("MAX_FILE_SIZE", "2097152");
+
+#allowed extension...
+$ext = ["image/jpg","image/jpeg", "image/png"];
+
+if(array_key_exists('save', $_POST)) {
+	$errors = [];
+
+	#be sure a file was selected....
+	if(empty($_FILES['pic']['name'])){
+		$errors[]= "please choose a file";
+	}
+
+	#check file size...
+
+	if($_FILES['pic']['size'] > MAX_FILE_SIZE) {
+		$errors[] = "file size exceeds maximum. maximum: ".MAX_FILE_SIZE;
+	}
+
+	#check extention....
+	if(!in_array($_FILES['pic']['type'], $ext)) {
+		$errors[] = "Invalid file type";
+	}
+
+	#generate random number to append
+	$rnd = rand(0000000000, 9999999999);
+
+	#strip filename for spaces
+
+	$strip_name = str_replace("","_", $_FILES['pic']['name']);
+
+	$filename = $rnd.$strip_name;
+	$destination = 'uploads/'.$filename;
+
+	if(!move_uploaded_file($_FILES['pic']['tmp_name'], $destination)) {
+		$errors[]  = "file upload failed";
+	}
+
+
+
+	if(empty($errors)) {
+		echo "done";
+
+	}else{
+		foreach ($errors as $error) {
+			echo $error. '</br>';
+		}
+	}
+}
 
 ?>
 
@@ -31,3 +81,4 @@ if(array_key_exists('save', $_POST)){
 
 	<input type="submit" name="save">
 </form>
+
