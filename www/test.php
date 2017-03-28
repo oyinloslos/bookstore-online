085921d7c6912477bb7170fea920c59d8d88aa87
Adedoyin Onabanjo <oyinorobo@gmail.com>
2017-03-28 16:00:22 +0100

    newly added

---
+++ www/test.php
@@ -0,0 +1,33 @@
+<?php  #test.php sandbox
+
+/*define('DBNAME','onlinestore');
+define('DBUSER','root');
+define('DBPASS','vagrant');
+
+$conn = new PDO ('mysql:host=localhost;dbname='.DBNAME, DBUSER,DBPASS);
+
+try{
+	#prepare a pdo instance
+	$conn = new pdo('mysql:host=localhost;dbname='.DBNAME, DBUSER, DBPASS);
+
+	#set verbose error modes
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+
+} catch(PDOException $e) {
+	echo $e->getMessage();
+}*/
+
+if(array_key_exists('save', $_POST)){
+	print_r($_FILES);
+}
+
+
+?>
+
+
+<form id="register" method="POST" enctype="multipart/form-data">
+	<p>please upload a file</p>
+	<input type="file" name="pic">
+
+	<input type="submit" name="save">
+</form>
